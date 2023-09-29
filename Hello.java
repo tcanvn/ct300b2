d410b009a932371b690601fb54c122e7d04870b5
Tran Cong An <tcanvn@gmail.com>
2023-09-29 13:54:20 +0700

    modified Hello.java and created Hello.class

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-  public static vois main(String []args) {
-    Systen.out.println("Hello");
+  public static void main(String []args) {
+    System.out.println("Hello");
   }
 }
